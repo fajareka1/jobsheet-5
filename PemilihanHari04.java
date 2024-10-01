31f35b3b8b95b1013d962375d3ea5c6ab34a6144
fajareka1 <fajareka.haltec@gmail.com>
2024-10-01 22:20:50 +0700

    update : pemilihan hari case integer

--- PemilihanHari04.java
+++ PemilihanHari04.java
@@ -5,27 +5,50 @@ public class PemilihanHari04 {
         Scanner sc = new Scanner(System.in);
 
         String dayName, dayType;
+        int dayInt;
 
-        System.out.print("Input day name : ");
-        dayName = sc.nextLine();
+        // System.out.print("Input day name : ");
+        // dayName = sc.nextLine();
 
-        switch (dayName.toLowerCase()) {
-            case "monday":
-            case "tuesday":
-            case "wednesday":
-            case "thursday":
-            case "friday":
+        System.out.print("Input hari ke : ");
+        dayInt = sc.nextInt();
+
+        // switch (dayName.toLowerCase()) {
+        //     case "monday":
+        //     case "tuesday":
+        //     case "wednesday":
+        //     case "thursday":
+        //     case "friday":
+        //         dayType = "weekday";
+        //         break;
+        //     case "saturday":
+        //     case "sunday":
+        //         dayType = "weekend";
+        //         break;
+        //     default:
+        //         dayType = "Invalid day name";
+        //         break;
+        // }
+
+        switch (dayInt) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
                 dayType = "weekday";
                 break;
-            case "saturday":
-            case "sunday":
+            case 6:
+            case 7:
                 dayType = "weekend";
                 break;
             default:
-                dayType = "Invalid day name";
+                dayType = "Invalid number of day";
+                break;
         }
 
-        System.out.println(dayName + " is a " + dayType);
+        // System.out.println(dayName + " is a " + dayType);
+        System.out.println(dayInt + " is a " + dayType);
 
     }
 }
